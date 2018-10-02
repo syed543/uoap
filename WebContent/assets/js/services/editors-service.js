ee2bbfb684601992922823c3c5d631b44545402d
syed543 <syed.azher543@gmail.com>
2018-10-02 15:26:29 +0530

    - Editor changes

--- WebContent/assets/js/services/editors-service.js
+++ WebContent/assets/js/services/editors-service.js
@@ -5,20 +5,36 @@ define(['angular', 'services-module'], function(angular, services) {
 services.factory('EditorsService', ["Http", "$q", "$state", "$log", function (Http, $q, $state, $log) {
 		return {
 			getEditors: function() {
-        var deferred = $q.defer();
-        Http.getData('assets/data/editors.json').then(function(data){
-          deferred.resolve(data);
-        }).catch(function(err){});
-        return deferred.promise;
+                var deferred = $q.defer();
+                Http.getData('assets/data/editors.json').then(function(data){
+                  deferred.resolve(data);
+                }).catch(function(err){});
+                return deferred.promise;
 			},
-      addEditor: function(data) {
+            addEditor: function(data) {
 				var deferred = $q.defer();
-				/*HTTP.postData('assets/data/journals.json', data).then(function(data){*/
-        Http.getData('assets/data/editors.json', data).then(function(data){
-          deferred.resolve(data);
-        }).catch(function(err){});
-        return deferred.promise;
-			}
+                /*Http.postMultipartData('/addEditor', data).then(function(data){*/
+                Http.getData('assets/data/editors.json', data).then(function(data){
+                  deferred.resolve(data);
+                }).catch(function(err){});
+                return deferred.promise;
+			},
+            updateEditor: function(data) {
+                var deferred = $q.defer();
+                /*Http.postMultipartData('/addEditor', data).then(function(data){*/
+                Http.getData('assets/data/editors.json', data).then(function(data){
+                    deferred.resolve(data);
+                }).catch(function(err){});
+                return deferred.promise;
+            },
+            deleteEditor: function(editorEmail) {
+                var deferred = $q.defer();
+                /*HTTP.postData('assets/data/journals.json', reviewerId).then(function(data){*/
+                Http.getData('assets/data/editors.json', editorEmail).then(function(data){
+                    deferred.resolve(data);
+                }).catch(function(err){});
+                return deferred.promise;
+            }
 		};
 	}]);
 	return services;
