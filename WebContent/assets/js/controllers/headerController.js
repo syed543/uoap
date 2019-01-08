28560f3d7887b831d800c1f190ec9c5edfb23630
syed543 <syed.azher543@gmail.com>
2019-01-08 15:18:53 +0530

    journals menu on home page

--- WebContent/assets/js/controllers/headerController.js
+++ WebContent/assets/js/controllers/headerController.js
@@ -6,13 +6,24 @@ define(['angular',
         'controllers-module',
 		'angular-material'
         ], function(angular, controllers, ngMaterial) {
-controllers.controller("headerController", ["$scope", "$rootScope", "$state", "$stateParams", "$localStorage", "$sessionStorage", "authenticationSvc",
-  function($scope, $rootScope, $state, $stateParams, $localStorage, $sessionStorage, authenticationSvc) {
+controllers.controller("headerController", ["$scope", "$rootScope", "$state", "$stateParams", "$localStorage", "$sessionStorage", "authenticationSvc", "JournalsService",
+  function($scope, $rootScope, $state, $stateParams, $localStorage, $sessionStorage, authenticationSvc,  JournalsService) {
 
     $scope.logout = function() {
         authenticationSvc.logout();
         $state.go('login');
     }
+
+    var _getJournals = function() {
+      JournalsService.getJournals().then(function (data) {
+          if (data.statusCode == 200) { // Success
+              $scope.journals = data.data;
+          } else { 					// Error
+              console.log("Unable to fetch journals list. please contact support.");
+          }
+      });
+    };
+    _getJournals();
 
 }]);
 });
