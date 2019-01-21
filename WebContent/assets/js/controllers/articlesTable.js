7aef95f8a53118e7fd362467d6dd74dfc2883d0c
syed543 <syed.azher543@gmail.com>
2019-01-21 19:09:45 +0530

    article version and issueNo changes

--- WebContent/assets/js/controllers/articlesTable.js
+++ WebContent/assets/js/controllers/articlesTable.js
@@ -127,6 +127,9 @@ controllers.controller("articlesTableCtrl", ['$mdEditDialog', '$q', '$scope', '$
             $mdDialog.cancel();
         };
         $scope.journals = parentScope.journals;
+        $scope.versions = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,
+                            32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50];
+        $scope.issues = [1,2,3,4,5,6,7,8,9,10,11,12];
 
         $scope.uploadedFile = function(element) {
             $scope.$apply(function($scope) {
@@ -141,6 +144,8 @@ controllers.controller("articlesTableCtrl", ['$mdEditDialog', '$q', '$scope', '$
             _data['abstractDesc'] = $scope.article['abstractDesc'];
             _data['journalId'] = $scope.article['journalId'];
             _data['authors'] = $scope.article['authors'];
+            _data['issueNo'] = $scope.article['issueNo'];
+            _data['version'] = $scope.article['version'];
 
             fd.append("file", $scope.file);
             //angular.copy($scope.article, data);
