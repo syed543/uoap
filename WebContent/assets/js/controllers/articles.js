fa0729b82bf3c87e15abcd9adff100b482c5fceb
Syed Azher <syed.azher543@gmail.com>
2020-07-08 13:45:40 +0000

    ui changes for article lists

--- WebContent/assets/js/controllers/articles.js
+++ WebContent/assets/js/controllers/articles.js
@@ -6,8 +6,8 @@ define(['angular',
         'controllers-module',
 		'angular-material'
         ], function(angular, controllers, ngMaterial) {
-controllers.controller("articlesCtrl", ['$mdEditDialog', '$q', '$scope', '$timeout', 'ArticlesService',
-  function($mdEditDialog, $q, $scope, $timeout, ArticlesService) {
+controllers.controller("articlesCtrl", ['$mdEditDialog', '$q', '$scope', '$timeout', 'ArticlesService', '$mdDialog',
+  function($mdEditDialog, $q, $scope, $timeout, ArticlesService, $mdDialog) {
 
 
     $scope.refreshArticles = function() {
@@ -28,6 +28,22 @@ controllers.controller("articlesCtrl", ['$mdEditDialog', '$q', '$scope', '$timeo
     $scope.downloadArticle = function(article) {
         ArticlesService.downloadArticle(article.id)
     };
+
+    $scope.showAbstract = function(ev, article) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    // Modal dialogs should fully cover application
+    // to prevent interaction outside of dialog
+    $mdDialog.show(
+      $mdDialog.alert()
+        .parent(angular.element(document.querySelector('#articlesList')))
+        .clickOutsideToClose(true)
+        .title(article.title)
+        .textContent(article.abstractDesc)
+        .ariaLabel('Abstract')
+        .ok('Ok')
+        .targetEvent(ev)
+    );
+  };
   
 }]);
 });
